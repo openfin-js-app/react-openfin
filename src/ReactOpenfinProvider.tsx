9db7d199cad7be9db002e6f677a27662c3ae9b45
Albert Li <liwentao90@yahoo.com>
2019-03-25 23:44:41 -0400

    init setup structure 2#
    - add redux-openfin middleware

--- src/ReactOpenfinProvider.tsx
+++ src/ReactOpenfinProvider.tsx
@@ -4,6 +4,8 @@ import { useEffect } from 'react'
 import i18n  from 'i18next';
 import {History} from 'history'
 
+import {IDockingOptions} from 'redux-openfin/docking/DockingType'
+
 import {ILaunchBarItem} from './GlobalTypes'
 import {setInitState, resetInitState} from './init';
 
@@ -12,15 +14,24 @@ import { IConfigTab } from './reduxs'
 import RootReduxProvider from './rootRedux/RootReduxProvider'
 
 interface IProps {
+
+    fin:any,
+    finUuid: string,
+    sharedActions: string[],
+    enableAutoDocking:boolean,
+    dockingOptions:IDockingOptions,
+
     i18n:typeof i18n,
     hist:History,
     launchBarItems:ILaunchBarItem[],
     configTabs:IConfigTab[],
+
 }
 
 const ReactOpenfinProvider:React.FunctionComponent<IProps> = (
     {
         children,
+        fin, finUuid, sharedActions, enableAutoDocking, dockingOptions,
         i18n, hist, launchBarItems, configTabs,
 
     }
@@ -34,7 +45,15 @@ const ReactOpenfinProvider:React.FunctionComponent<IProps> = (
     });
 
     return(<React.Fragment>
-        <RootReduxProvider>
+        <RootReduxProvider
+            fin={fin}
+            finMiddlewareConfig={{
+                finUuid,
+                sharedActions,
+                autoDocking:enableAutoDocking,
+                dockingOptions,
+            }}
+        >
             { children }
         </RootReduxProvider>
     </React.Fragment>)
--- src/rootRedux/RootReduxProvider.tsx
+++ src/rootRedux/RootReduxProvider.tsx
@@ -1,16 +1,28 @@
 import * as React from 'react';
 
+import { createOpenfinMiddleware } from 'redux-openfin';
+import { IConfig } from 'redux-openfin/init'
 import { useEnhancedReducerAndSaga } from '../utils/useEnhancedReducerAndSaga'
 import rootReducer, {IRootState, buildInitState} from '../reduxs'
 import rootSaga from '../reduxs/sagas'
 
 import { RootReduxContextProvider } from './RootReduxContext'
 
-const RootReduxProvider:React.FunctionComponent<{}> = (
-    { children }
+interface IProps {
+    fin:any,
+    finMiddlewareConfig:IConfig,
+}
+
+const RootReduxProvider:React.FunctionComponent<IProps> = (
+    {
+        children,
+        fin,finMiddlewareConfig,
+    }
 )=>{
 
-    const [state, dispatch] = useEnhancedReducerAndSaga(rootReducer,buildInitState(),[],rootSaga,{});
+    const [state, dispatch] = useEnhancedReducerAndSaga(rootReducer,buildInitState(),[
+        createOpenfinMiddleware(fin,finMiddlewareConfig)
+    ],rootSaga,{});
 
     return(<React.Fragment>
         <RootReduxContextProvider value={{state,dispatch}}>
