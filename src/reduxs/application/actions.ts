162311166c4edfd80267a60350dabd66c13e1e52
Albert Li <liwentao90@yahoo.com>
2019-03-28 23:19:15 -0400

    milestone3.1# Reactor act generator types 1#

--- src/reduxs/application/actions.ts
+++ src/reduxs/application/actions.ts
@@ -1,60 +1,75 @@
 import { createAction, ActionFunctionAny, Action } from 'redux-actions';
 import {WindowOptions} from 'redux-openfin';
 
+import makeType, {makeResType, makeReqType} from '../../utils/makeType'
+
 import { IApplicationNewSnackbarOption, IApplicationCloseSnackBarOption } from './types';
 import * as React from "react";
 
-export const APPLICATION_SET_LOADING_MSG:string = 'APPLICATION_SET_LOADING_MSG';
-export const APPLICATION_STARTED:string = 'APPLICATION_STARTED';
-export const APPLICATION_CHILD_STARTED:string = 'APPLICATION_CHILD_STARTED';
-export const APPLICATION_NOTIFICATION_STARTED:string = 'APPLICATION_NOTIFICATION_STARTED';
-export const APPLICATION_READY:string = 'APPLICATION_READY';
-export const APPLICATION_DRAWER_TOGGLE:string = 'APPLICATION_DRAWER_TOGGLE';
-export const APPLICATION_TOGGLE_WINDOW_STATE:string = 'APPLICATION_TOGGLE_WINDOW_STATE';
+export const APPLICATION_SET_LOADING_MSG        = makeType('APPLICATION_SET_LOADING_MSG');
+export const APPLICATION_STARTED                = makeType('APPLICATION_STARTED');
+export const APPLICATION_CHILD_STARTED          = makeType('APPLICATION_CHILD_STARTED');
+export const APPLICATION_NOTIFICATION_STARTED   = makeType('APPLICATION_NOTIFICATION_STARTED');
+export const APPLICATION_READY                  = makeType('APPLICATION_READY');
+export const APPLICATION_DRAWER_TOGGLE          = makeType('APPLICATION_DRAWER_TOGGLE');
+export const APPLICATION_TOGGLE_WINDOW_STATE    = makeType('APPLICATION_TOGGLE_WINDOW_STATE');
 
-export const applicationSetLoadingMsg:ActionFunctionAny<Action<{loadingMsg:string}>> =
-    createAction<{loadingMsg:string},string>(APPLICATION_SET_LOADING_MSG,(loadingMsg:string)=>({loadingMsg}));
-export const applicationStarted:ActionFunctionAny<Action<void>> = createAction(APPLICATION_STARTED);
-export const applicationChildStarted:ActionFunctionAny<Action<void>> = createAction(APPLICATION_CHILD_STARTED);
-export const applicationNotificationStarted:ActionFunctionAny<Action<void>> = createAction(APPLICATION_NOTIFICATION_STARTED);
-export const applicationReady:ActionFunctionAny<Action<void>> = createAction(APPLICATION_READY);
-export const applicationDrawerToggle:ActionFunctionAny<Action<void>> = createAction(APPLICATION_DRAWER_TOGGLE);
-export const applicationToogleWindowState:ActionFunctionAny<Action<void>> = createAction(APPLICATION_TOGGLE_WINDOW_STATE);
+export const applicationSetLoadingMsg
+    = createAction<{loadingMsg:string},string>(APPLICATION_SET_LOADING_MSG,(loadingMsg:string)=>({loadingMsg}));
+export const applicationStarted
+    = createAction<void>(APPLICATION_STARTED);
+export const applicationChildStarted:ActionFunctionAny<Action<void>>
+    = createAction(APPLICATION_CHILD_STARTED);
+export const applicationNotificationStarted
+    = createAction<void>(APPLICATION_NOTIFICATION_STARTED);
+export const applicationReady
+    = createAction<void>(APPLICATION_READY);
+export const applicationDrawerToggle
+    = createAction<void>(APPLICATION_DRAWER_TOGGLE);
+export const applicationToogleWindowState
+    = createAction<void>(APPLICATION_TOGGLE_WINDOW_STATE);
 
 // snackbar
 
-export const APPLICATION_NEW_SNACKBAR='APPLICATION_NEW_SNACKBAR';
-export const APPLICATION_SET_SNACKBAR_STATUS='APPLICATION_SET_SNACKBAR_STATUS';
-export const APPLICATION_PROCESS_SNACKBAR_QUEUE='APPLICATION_PROCESS_SNACKBAR_QUEUE';
-export const APPLICATION_CLOSE_SNACKBAR='APPLICATION_CLOSE_SNACKBAR';
+export const APPLICATION_NEW_SNACKBAR               = makeType('APPLICATION_NEW_SNACKBAR');
+export const APPLICATION_SET_SNACKBAR_STATUS        = makeType('APPLICATION_SET_SNACKBAR_STATUS');
+export const APPLICATION_PROCESS_SNACKBAR_QUEUE     = makeType('APPLICATION_PROCESS_SNACKBAR_QUEUE');
+export const APPLICATION_CLOSE_SNACKBAR             = makeType('APPLICATION_CLOSE_SNACKBAR');
 
-export const applicationNewSnackbar:ActionFunctionAny<Action<IApplicationNewSnackbarOption>>
-    = createAction(APPLICATION_NEW_SNACKBAR, (option:IApplicationNewSnackbarOption)=>(option));
-export const applicationSetSnackbarStatus = createAction<{snackBarOpen:boolean},boolean>(APPLICATION_SET_SNACKBAR_STATUS, (snackBarOpen:boolean)=>({snackBarOpen}));
-export const applicationProcessSnackbarQueue:ActionFunctionAny<Action<{}>>
-    = createAction(APPLICATION_PROCESS_SNACKBAR_QUEUE);
+export const applicationNewSnackbar
+    = createAction<IApplicationNewSnackbarOption,IApplicationNewSnackbarOption>(APPLICATION_NEW_SNACKBAR, (option:IApplicationNewSnackbarOption)=>(option));
+export const applicationSetSnackbarStatus
+    = createAction<{snackBarOpen:boolean},boolean>(APPLICATION_SET_SNACKBAR_STATUS, (snackBarOpen:boolean)=>({snackBarOpen}));
+export const applicationProcessSnackbarQueue
+    = createAction<void>(APPLICATION_PROCESS_SNACKBAR_QUEUE);
 export const applicationCloseSnackbar:ActionFunctionAny<Action<IApplicationCloseSnackBarOption>>
-    = createAction(APPLICATION_CLOSE_SNACKBAR, (event: React.SyntheticEvent<any>, reason: string)=>({event,reason}));
+    = createAction<{event:React.SyntheticEvent<any>,reason:string},React.SyntheticEvent<any>, string>(
+        APPLICATION_CLOSE_SNACKBAR, (event: React.SyntheticEvent<any>, reason: string)=>({event,reason})
+);
 
 
 // snap dock
-export const APPLICATION_UPDATE_DOCK_STATUS = 'APPLICATION_UPDATE_DOCK_STATUS';
-export const applicationUpdateDockStatus:ActionFunctionAny<Action<{docked:boolean}>>
-    = createAction(APPLICATION_UPDATE_DOCK_STATUS, (docked:boolean)=>({docked}));
+export const APPLICATION_UPDATE_DOCK_STATUS             = makeType('APPLICATION_UPDATE_DOCK_STATUS');
+export const applicationUpdateDockStatus
+    = createAction<{docked},boolean>(APPLICATION_UPDATE_DOCK_STATUS, (docked:boolean)=>({docked}));
 
 // launch bar
-export const APPLICATION_LAUNCH_BAR_TOGGLE = 'APPLICATION_LAUNCH_BAR_TOGGLE';
-export const APPLICATION_LAUNCH_BAR_TOGGLE_COLLAPSE = 'APPLICATION_LAUNCH_BAR_TOGGLE_COLLAPSE';
-export const APPLICATION_LAUNCH_NEW_WINDOW = 'APPLICATION_LAUNCH_NEW_WINDOW';
-export const APPLICATION_LAUNCH_BAR_CLOSE = 'APPLICATION_LAUNCH_BAR_CLOSE';
-export const applicationLaunchBarToggle:ActionFunctionAny<Action<void>> = createAction(APPLICATION_LAUNCH_BAR_TOGGLE);
-export const applicationLaunchBarToggleCollapse:ActionFunctionAny<Action<void>> = createAction(APPLICATION_LAUNCH_BAR_TOGGLE_COLLAPSE);
-export const applicationLaunchNewWindow:ActionFunctionAny<Action<Partial<WindowOptions>>>
-    = createAction(APPLICATION_LAUNCH_NEW_WINDOW,(appJson:Partial<WindowOptions>)=>(appJson));
+export const APPLICATION_LAUNCH_BAR_TOGGLE              = makeType('APPLICATION_LAUNCH_BAR_TOGGLE');
+export const APPLICATION_LAUNCH_BAR_TOGGLE_COLLAPSE     = makeType('APPLICATION_LAUNCH_BAR_TOGGLE_COLLAPSE');
+export const APPLICATION_LAUNCH_NEW_WINDOW              = makeType('APPLICATION_LAUNCH_NEW_WINDOW');
+export const APPLICATION_LAUNCH_BAR_CLOSE               = makeType('APPLICATION_LAUNCH_BAR_CLOSE');
+export const applicationLaunchBarToggle
+    = createAction<void>(APPLICATION_LAUNCH_BAR_TOGGLE);
+export const applicationLaunchBarToggleCollapse
+    = createAction<void>(APPLICATION_LAUNCH_BAR_TOGGLE_COLLAPSE);
+export const applicationLaunchNewWindow
+    = createAction<Partial<WindowOptions>,Partial<WindowOptions>>(APPLICATION_LAUNCH_NEW_WINDOW,(appJson:Partial<WindowOptions>)=>(appJson));
 export const applicationLaunchBarClose:ActionFunctionAny<Action<void>> = createAction(APPLICATION_LAUNCH_BAR_CLOSE);
 
 // network
-export const APPLICATION_NETWORK_ONLINE = 'APPLICATION_NETWORK_ONLINE';
-export const applicationNetworkOnline:ActionFunctionAny<Action<void>> = createAction(APPLICATION_NETWORK_ONLINE);
-export const APPLICATION_NETWORK_OFFLINE = 'APPLICATION_NETWORK_OFF_LINE';
-export const applicationNetworkOffline:ActionFunctionAny<Action<void>> = createAction(APPLICATION_NETWORK_OFFLINE);
+export const APPLICATION_NETWORK_ONLINE                 = makeType('APPLICATION_NETWORK_ONLINE');
+export const applicationNetworkOnline
+    = createAction<void>(APPLICATION_NETWORK_ONLINE);
+export const APPLICATION_NETWORK_OFFLINE                = makeType('APPLICATION_NETWORK_OFF_LINE');
+export const applicationNetworkOffline
+    = createAction<void>(APPLICATION_NETWORK_OFFLINE);
--- src/reduxs/config/actions.ts
+++ src/reduxs/config/actions.ts
@@ -1,5 +1,7 @@
 import {createAction, ActionFunctionAny, Action} from 'redux-actions';
 
+import makeType, {makeResType, makeReqType} from '../../utils/makeType'
+
 import {
     IConfigDexie,
     IConfigResetOption,
@@ -8,43 +10,43 @@ import {
     IConfigDoUpdateOneFieldOption
 } from './types';
 
-export const CONFIG_RESET:string = 'CONFIG_RESET';
+export const CONFIG_RESET                           = makeType('CONFIG_RESET');
 
 // redux
-export const CONFIG_DO_UPDATE_ONE_FIELD:string = 'CONFIG_DO_UPDATE_ONE_FIELD';
-export const CONFIG_UPDATE_GLOBAL_FILTER_STR:string = 'CONFIG_UPDATE_GLOBAL_FILTER_STR';
-export const configReset:ActionFunctionAny<Action<IConfigResetOption>>
-    = createAction(CONFIG_RESET, (option)=>(option));
-export const configDoUpdateOneField:ActionFunctionAny<Action<IConfigDoUpdateOneFieldOption>>
-    = createAction(CONFIG_DO_UPDATE_ONE_FIELD, (option)=>(option));
-export const configUpdateGlobalFilterStr:ActionFunctionAny<Action<IConfigUpdateGlobalFilterStrOption>>
-    = createAction(CONFIG_UPDATE_GLOBAL_FILTER_STR, (option)=>(option));
+export const CONFIG_DO_UPDATE_ONE_FIELD             = makeType('CONFIG_DO_UPDATE_ONE_FIELD');
+export const CONFIG_UPDATE_GLOBAL_FILTER_STR        = makeType('CONFIG_UPDATE_GLOBAL_FILTER_STR');
+export const configReset
+    = createAction<IConfigResetOption,IConfigResetOption>(CONFIG_RESET, (option:IConfigResetOption)=>(option));
+export const configDoUpdateOneField
+    = createAction<IConfigDoUpdateOneFieldOption,IConfigDoUpdateOneFieldOption>(CONFIG_DO_UPDATE_ONE_FIELD, (option:IConfigDoUpdateOneFieldOption)=>(option));
+export const configUpdateGlobalFilterStr
+    = createAction<IConfigUpdateGlobalFilterStrOption,IConfigUpdateGlobalFilterStrOption>(CONFIG_UPDATE_GLOBAL_FILTER_STR, (option:IConfigUpdateGlobalFilterStrOption)=>(option));
 
 // saga
-export const CONFIG_LOAD_FROM_DEXIE:string = 'CONFIG_LOAD_FROM_DEXIE';
-export const configLoadFromDexie:ActionFunctionAny<Action<void>>
-    = createAction(CONFIG_LOAD_FROM_DEXIE,);
-export const CONFIG_DO_UPDATE_ONE_FIELD_IN_DEXIE:string = 'CONFIG_DO_UPDATE_ONE_FIELD_IN_DEXIE';
-export const configDoUpdateOneFieldInDexie:ActionFunctionAny<Action<IConfigDexie>>
-    = createAction(CONFIG_DO_UPDATE_ONE_FIELD_IN_DEXIE, (option)=>(option));
+export const CONFIG_LOAD_FROM_DEXIE                 = makeType('CONFIG_LOAD_FROM_DEXIE');
+export const configLoadFromDexie
+    = createAction<void>(CONFIG_LOAD_FROM_DEXIE);
+export const CONFIG_DO_UPDATE_ONE_FIELD_IN_DEXIE    = makeType('CONFIG_DO_UPDATE_ONE_FIELD_IN_DEXIE');
+export const configDoUpdateOneFieldInDexie
+    = createAction<IConfigDexie,IConfigDexie>(CONFIG_DO_UPDATE_ONE_FIELD_IN_DEXIE, (option:IConfigDexie)=>(option));
 
-export const CONFIG_UPDATE_ONE_FIELD:string = 'CONFIG_UPDATE_ONE_FIELD';
-export const configUpdateOneField:ActionFunctionAny<Action<IConfigUpdateOneFieldOption>>
-    = createAction(CONFIG_UPDATE_ONE_FIELD, (option)=>(option));
+export const CONFIG_UPDATE_ONE_FIELD                = makeType('CONFIG_UPDATE_ONE_FIELD');
+export const configUpdateOneField
+    = createAction<IConfigUpdateOneFieldOption,IConfigUpdateOneFieldOption>(CONFIG_UPDATE_ONE_FIELD, (option:IConfigUpdateOneFieldOption)=>(option));
 
 
 
 // optional actions depending on the config application values
 
-export const CONFIG_UPDATE_NEW_WINDOW_POSITION:string = 'CONFIG_UPDATE_NEW_WINDOW_POSITION';
-export const CONFIG_UPDATE_NEW_WINDOW_POSITION_ADD_DELTA:string = 'CONFIG_UPDATE_NEW_WINDOW_POSITION_ADD_DELTA';
-export const CONFIG_UPDATE_NEW_WINDOW_POSITION_RESET_TOP:string = 'CONFIG_UPDATE_NEW_WINDOW_POSITION_RESET_TOP';
-export const CONFIG_UPDATE_NEW_WINDOW_POSITION_RESET_LEFT:string = 'CONFIG_UPDATE_NEW_WINDOW_POSITION_RESET_LEFT';
-export const configUpdateNewWindowPosition:ActionFunctionAny<Action<void>>
-    = createAction(CONFIG_UPDATE_NEW_WINDOW_POSITION);
-export const configUpdateNewWindowPositionAddDelta:ActionFunctionAny<Action<void>>
-    = createAction(CONFIG_UPDATE_NEW_WINDOW_POSITION_ADD_DELTA);
-export const configUpdateNewWindowPositionResetTop:ActionFunctionAny<Action<void>>
-    = createAction(CONFIG_UPDATE_NEW_WINDOW_POSITION_RESET_TOP);
-export const configUpdateNewWindowPositionResetLeft:ActionFunctionAny<Action<void>>
-    = createAction(CONFIG_UPDATE_NEW_WINDOW_POSITION_RESET_LEFT);
+export const CONFIG_UPDATE_NEW_WINDOW_POSITION              = makeType('CONFIG_UPDATE_NEW_WINDOW_POSITION');
+export const CONFIG_UPDATE_NEW_WINDOW_POSITION_ADD_DELTA    = makeType('CONFIG_UPDATE_NEW_WINDOW_POSITION_ADD_DELTA');
+export const CONFIG_UPDATE_NEW_WINDOW_POSITION_RESET_TOP    = makeType('CONFIG_UPDATE_NEW_WINDOW_POSITION_RESET_TOP');
+export const CONFIG_UPDATE_NEW_WINDOW_POSITION_RESET_LEFT   = makeType('CONFIG_UPDATE_NEW_WINDOW_POSITION_RESET_LEFT');
+export const configUpdateNewWindowPosition
+    = createAction<void>(CONFIG_UPDATE_NEW_WINDOW_POSITION);
+export const configUpdateNewWindowPositionAddDelta
+    = createAction<void>(CONFIG_UPDATE_NEW_WINDOW_POSITION_ADD_DELTA);
+export const configUpdateNewWindowPositionResetTop
+    = createAction<void>(CONFIG_UPDATE_NEW_WINDOW_POSITION_RESET_TOP);
+export const configUpdateNewWindowPositionResetLeft
+    = createAction<void>(CONFIG_UPDATE_NEW_WINDOW_POSITION_RESET_LEFT);
