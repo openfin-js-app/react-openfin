148e85068d4d215b23af4a63ab4a23b536dca034
Wentao LI <liwentao90@yahoo.com>
2019-04-07 21:53:04 -0400

    remove unneeded setupTests

--- src/setupTests.ts
+++
@@ -1,5 +0,0 @@
-import Dexie from 'dexie';
-import indexedDB from 'fake-indexeddb';
-
-Dexie.dependencies.indexedDB = indexedDB;
-Dexie.dependencies.IDBKeyRange = require('fake-indexeddb/lib/FDBKeyRange');
