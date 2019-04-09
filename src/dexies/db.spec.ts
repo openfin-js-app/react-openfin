a4730896f787c81523becfdb3435e2f231f8f6cf
Wentao LI <liwentao90@yahoo.com>
2019-04-09 10:12:18 -0400

    - restore the dexies config test cases

--- src/dexies/db.spec.ts
+++ src/dexies/db.spec.ts
@@ -1,9 +1,3 @@
-import Dexie from 'dexie';
-import indexedDB from 'fake-indexeddb';
-
-Dexie.dependencies.indexedDB = indexedDB;
-Dexie.dependencies.IDBKeyRange = require('fake-indexeddb/lib/FDBKeyRange');
-
 import db from './db';
 
 
