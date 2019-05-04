57b6e8111fab46463b93c6ebea96ca54ffc16dc9
Wentao LI <liwentao90@yahoo.com>
2019-05-04 00:06:13 -0400

    - initially add APPLICATION_LAUNCH_BAR_TOGGLED response action 2#

--- src/reduxs/sharedActions.ts
+++ src/reduxs/sharedActions.ts
@@ -1,11 +1,13 @@
 import {
     APPLICATION_SET_LOADING_MSG,
     APPLICATION_LAUNCH_NEW_WINDOW,
+    APPLICATION_LAUNCH_BAR_TOGGLED,
 } from './application/actions';
 
 const sharedActions:string[]=[
     APPLICATION_SET_LOADING_MSG,
     APPLICATION_LAUNCH_NEW_WINDOW,
+    APPLICATION_LAUNCH_BAR_TOGGLED,
 ];
 
 export default sharedActions;
