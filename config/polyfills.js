48567f8222eab2a6755aca688d5ff3755a16f5da
Wentao LI <liwentao90@yahoo.com>
2019-06-05 14:42:23 -0400

    initially 1#
    - Update dependencies to
        - @material-ui/core @ ^4.0.2
        - create-react-app @ 3.0.1
    - Remove dependencies
        - material-ui-pickers @ ^2.2.4

--- config/polyfills.js
+++ config/polyfills.js
@@ -9,11 +9,11 @@ if (typeof Promise === 'undefined') {
 }
 
 // fetch() polyfill for making API calls.
-require('whatwg-fetch');
+// require('whatwg-fetch');
 
 // Object.assign() is commonly used with React.
 // It will use the native implementation if it's present and isn't buggy.
-Object.assign = require('object-assign');
+// Object.assign = require('object-assign');
 
 // In tests, polyfill requestAnimationFrame since jsdom doesn't provide it yet.
 // We don't polyfill it in the browser--this is user's responsibility.
